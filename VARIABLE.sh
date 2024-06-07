23b38a81bf40e9aacc338b13a31726743a8d8797
dong881 <joe888joejoe@gmail.com>
2024-06-07 10:17:47 +0200

    Fix issue with multiple users building and running the code simultaneously

--- VARIABLE.sh
+++ VARIABLE.sh
@@ -1,1 +1,4 @@
-export PATH_TO_openairinterface5g="/home/chen/openairinterface5g"
+RROT_PATH="/home/chen"
+export PATH_TO_openairinterface5g="$RROT_PATH/openairinterface5g"
+# export LOCKFILE="$RROT_PATH/nfapi-split-script/tmp-build_run.lock"
+
