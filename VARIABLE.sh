92bde8dd7a2ee53f238ac77e4cd320ae04fc9025
dong881 <joe888joejoe@gmail.com>
2024-07-27 16:57:38 +0800

    reduce pacp size only lo and SCTP, UDP

--- VARIABLE.sh
+++ VARIABLE.sh
@@ -1,3 +1,3 @@
 ROOT_PATH="hpe"
-export PATH_TO_openairinterface5g="/home/$ROOT_PATH/openairinterface5g"
+export PATH_TO_openairinterface5g="/home/$ROOT_PATH/OnlyOAI/openairinterface5g"
 export PATH_TO_SCRIPT="/home/$ROOT_PATH/OnlyOAI/nfapi-split-script"
