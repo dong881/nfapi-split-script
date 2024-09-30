763e7ad2204bfbfbf445a34448c61b07fb573989
dong881 <joe888joejoe@gmail.com>
2024-09-30 18:19:45 +0800

    Update var path setting

--- VARIABLE.sh
+++ VARIABLE.sh
@@ -1,3 +1,5 @@
 ROOT_PATH="hpe"
 export PATH_TO_openairinterface5g="/home/$ROOT_PATH/OnlyOAI/openairinterface5g"
+export PATH_TO_PNF="/home/$ROOT_PATH/openairinterface5g"
+export PATH_TO_UE="/home/$ROOT_PATH/openairinterface5g"
 export PATH_TO_SCRIPT="/home/$ROOT_PATH/OnlyOAI/nfapi-split-script"
--- set_oai_path.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Set OAI_PATH environment variable
-export OAI_PATH=~/openairinterface5g
